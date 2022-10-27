87e31b49d2ae360118e5b9f1f85cc9b23d97fbbe
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-27 20:51:12 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name, age, sex,color,temperament,alive)
+VALUES
+    ("Mr. Chocolate","12","M","white","melachoric","1"),
+    ( "Rowdy", "25", "F", "black", "senguine", "1"),
+    ( "Tabitha", "50", "F", "brown", "melancholic", "1"),
+    ( "Sergeant Brown", "35", "M", "brown", "phlegmatic", "1"),
+    ( "Melissa", "15", "F", "white", "choleric", "0"),
+    ( "Grinch", "15", "M", "black", "phlegmatic", "1"),
+    ( "Wendy", "15", "F", "black", "senguine", "1");
+    INSERT INTO bears (name, age, sex,color,temperament,alive)
+    VALUES
+    (Null, "7" , "F", "black","phlegmatic", "0");
